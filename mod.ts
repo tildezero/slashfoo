5e0d73f8026d2042b097738fe983366fc7a98d79
suhas <61701181+tildezero@users.noreply.github.com>
2022-02-12 20:47:13 -0600

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,28 @@
+import * as deploy from "https://code.harmony.rocks/v2.5.1/deploy.ts"
+
+deploy.init({ env: true })
+
+const commands = await deploy.commands.all()
+if (commands.size !== 1) {
+  deploy.commands.bulkEdit([
+    {
+      name: 'ping',
+      description: "It's literally ping command. What did you expect?",
+      options: [
+        {
+          name: 'pingarg',
+          description: 'Again literally pingArg',
+          required: false,
+          type: deploy.ApplicationCommandOptionType.STRING
+        }
+      ]
+    }
+  ])
+}
+
+deploy.handle('ping', (d) => {
+  const arg = d.option<string | undefined>('pingarg')
+  d.reply(`Pong! You typed: ${arg !== undefined ? arg : 'nothing'}`)
+})
+
+console.log("bot running")
